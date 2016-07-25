f4947b6146d1fc1959923018ba8192f52fe43bce
Jeshua Maxey <me@jeshua.co>
2016-07-25 20:14:51 +0100

    Refactor Digit
    Refactor Digit class definition

--- js/Digit.js
+++ js/Digit.js
@@ -1,12 +1,10 @@
 var Digit = function(ctx, pos, radius) {
-  var self = this;
-
   var NUMBER_OF_ROWS = 6;
   var NUMBER_OF_CLOCKS_PER_ROW = 4;
 
   this.ctx = ctx;
   this.pos = pos || {x: 0, y: 0};
-  self.radius = radius || 40;
+  this.radius = radius || 40;
   this.numeral = 0;
   this.clockfaces = [];
 
@@ -14,33 +12,32 @@ var Digit = function(ctx, pos, radius) {
   for(var i = 0; i < NUMBER_OF_ROWS; i++) {
    for(var j = 0; j < NUMBER_OF_CLOCKS_PER_ROW; j++) {
      clockfacePos = {
-       x: this.pos.x + (j+0.5)*2*self.radius,
-       y: this.pos.y + (i+0.5)*2*self.radius
+       x: this.pos.x + (j+0.5)*2*this.radius,
+       y: this.pos.y + (i+0.5)*2*this.radius
      };
-     this.clockfaces.push(new ClockFace(this.ctx, clockfacePos, self.radius));
+     this.clockfaces.push(new ClockFace(this.ctx, clockfacePos, this.radius));
    }
   }
-
-  this.draw = function(duration) {
-    var drawMethod = duration ? 'animateToQueued' : 'draw';
-    this.clockfaces.forEach(function(clockface) {
-      clockface[drawMethod](duration);
-    });
-  };
-  
-  this._numeralSetter = function() {
-    this.clockfaces.forEach(function(clockface, i) {
-      // clockface.setHands(NUMERALS[self.numeral][i]);
-      clockface.queue(NUMERALS[self.numeral][i]);
-    });
-  }
-  
-  this.setNumeral = function(n) {
-    if(n > 9 || n < 0 || n%1) {
-      throw new Error('The numeral must be an integer from 0-9');
-    } else {
-      self.numeral = n;
-      this._numeralSetter.call(self);
-    } 
-  };
 }
+
+Digit.prototype.draw = function(duration) {
+  var drawMethod = duration ? 'animateToQueued' : 'draw';
+  this.clockfaces.forEach(function(clockface) {
+    clockface[drawMethod](duration);
+  });
+};
+
+Digit.prototype._numeralSetter = function() {
+  this.clockfaces.forEach(function(clockface, i) {
+    clockface.queue(NUMERALS[this.numeral][i]);
+  }.bind(this));
+};
+
+Digit.prototype.setNumeral = function(n) {
+  if(n > 9 || n < 0 || n%1) {
+    throw new Error('The numeral must be an integer from 0-9');
+  } else {
+    this.numeral = n;
+    this._numeralSetter();
+  }
+};
